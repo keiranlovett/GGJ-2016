bf66a10350aeb5638d178dc622f0e281979dbdec
Keiran Lovett <kvclovett@gmail.com>
2016-01-30 18:38:07 +0800

    Preliminary networking of animation triggers

--- Assets/Scripts/Player/ActorController.cs
+++ Assets/Scripts/Player/ActorController.cs
@@ -14,7 +14,6 @@ public enum Team
 public class ActorController : MonoBehaviour {
 
 	public static ActorController actor;
-
 	Animator animator;
 
 	public bool isControlledLocally;
@@ -25,6 +24,7 @@ public class ActorController : MonoBehaviour {
 	public BodyPhysicsController ragdollController;
 	float rotationSpeed = 15f;
 
+	//Local Variables
 	Vector3 inputVec;
 	Vector3 targetDirection;
 	Vector3 targetDashDirection;
@@ -46,8 +46,17 @@ public class ActorController : MonoBehaviour {
 	private RaycastHit hit;
  	private Vector3 dir = Vector3.up;
 
+ 	//Networked Variables
+ 	public Vector2 inputPos;
+ 	public bool inputJump;
+ 	public bool inputAttack0;
+ 	public bool inputAttack1;
+ 	public bool inputAttack2;
+ 	public bool inputAttack3;
+  	public bool inputLightHit;
 
-	PhotonView m_View;
+
+ 	PhotonView m_View;
 	public PhotonView PhotonView
 	{
 		get
@@ -65,8 +74,6 @@ public class ActorController : MonoBehaviour {
 	}
 
 	void Update(){
-
-
 		if (Physics.Raycast(transform.position, -Vector3.up, out hit)) {
 			if(hit.distance > 2) {
 				Debug.Log(hit.distance + "In Air?: " + isInAir);
@@ -75,11 +82,6 @@ public class ActorController : MonoBehaviour {
 					ragdollController.m_currOffBalance = ragdollController.m_balance;
 				}
 			}
-		}
-
-		if(isControlledLocally) {
-		} else {
-			return;
 		}
 
 		if(ragdollController.currState_ == BodyPhysicsController.State.FALLING) {
@@ -91,22 +93,30 @@ public class ActorController : MonoBehaviour {
 		GameObject[] enemies = GameObject.FindGameObjectsWithTag("Player");
 		target = GetClosestEnemy(enemies);
 
-		if(Input.GetKeyDown(KeyCode.P)){
-			Debug.Break();
-		}
-
+		//Normalise back to 0
 		inputVec = new Vector3(0,0,0);
 
-		if(!dead){
+		//If we're not dead, we can control the player
+		//Anything locally controlled goes here!
+		if(isControlledLocally && !dead) {
+
 			animator.SetBool("Block", false);
 
 			//Get input from controls
 			float z = Input.GetAxisRaw("Horizontal");
 			float x = -(Input.GetAxisRaw("Vertical"));
-			inputVec = new Vector3(x, 0, z);
+			inputPos = new Vector3(z,x);
+
+			if(!isControlledLocally) {
+				inputVec = new Vector3(inputPos.x, 0, inputPos.y);
+			} else {
+				inputVec = new Vector3(x, 0, z);
+			}
+
 			//Apply inputs to animator
 			animator.SetFloat("Input X", z);
 			animator.SetFloat("Input Z", -(x));
+
 			if (x > .1 || x < -.1 || z > .1 || z < -.1){  //if there is some input (account for controller deadzone)
 				//set that character is moving
 				animator.SetBool("Moving", true);
@@ -126,132 +136,179 @@ public class ActorController : MonoBehaviour {
 				animator.SetBool("Running", false);
 				isMoving = false;
 			}
-
-			if (Input.GetButtonDown("Jump")){
-				if(isStrafing){
-					animator.SetTrigger("JumpTrigger");
-					StartCoroutine(COSetInAir(.3f, .4f));
-				}
-				if(isMoving){
-					animator.SetTrigger("JumpForwardTrigger");
-					StartCoroutine(COSetInAir(0f, 4f));
-				}else{
-			    	animator.SetTrigger("JumpTrigger");
-					StartCoroutine(COSetInAir(.3f, .4f));
-				}
-			}
-			if (Input.GetButtonDown("Fire0")){
-				animator.SetTrigger("RangeAttack1Trigger");
-				StartCoroutine (COStunPause(1.2f));
-			}
-			if (Input.GetButtonDown("Fire1")){
-				if(!canChain){ //used for characters who can chain attacks to chain to 2nd Attack
-					animator.SetTrigger("Attack1Trigger");
-					StartCoroutine(COChainWindow(.2f, .4f));
-					StartCoroutine (COStunPause(.6f));
-					chain1 = true;
-					chain2 = false;
-				}
-				else{
-					if(chain1){  //if within chain time do ATTACK2
-						animator.SetTrigger("Attack2Trigger");
-						StopAllCoroutines();
-
-						StartCoroutine(COChainWindow(.1f, 2f));
-						chain1 = false;
-						chain2 = true;
-					}
-					else if(chain2){
-						StopAllCoroutines();
-						animator.SetTrigger("Attack3Trigger");
-						chain1 = false;
-						chain2 = false;
-						StartCoroutine (COStunPause(1.2f));
-						canChain = false;
-					}
-				}
-			}
-			if (Input.GetButtonDown("Fire2")){
-				animator.SetTrigger("MoveAttack1Trigger");
-				StartCoroutine (COStunPause(.9f));
-			}
-			if (Input.GetButtonDown("Fire3")){
-				animator.SetTrigger("SpecialAttack1Trigger");
-				StartCoroutine (COStunPause(1.7f));
-			}
-			if (Input.GetButtonDown("LightHit")){
-				animator.SetTrigger("LightHitTrigger");
-				StartCoroutine (COStunPause(2.8f));
-			}
-			if(Input.GetAxis("DashVertical") > .5 || Input.GetAxis("DashVertical") < -.5 || Input.GetAxis("DashHorizontal") > .5 || Input.GetAxis("DashHorizontal") < -.5){
-				StartCoroutine (CODirectionalDash(Input.GetAxis("DashVertical"), Input.GetAxis("DashHorizontal")));
-			}
-			if(Input.GetButtonDown("Special")) {
-				Debug.Log("Special");
-				if (!isStealth){
-					isStealth = true;
-					animator.SetBool("Stealth", true);
-				} else {
-					isStealth = false;
-					animator.SetBool("Stealth", false);
-				}
-			}
 		} else {
 			animator.SetBool("Block", true);
 			animator.SetFloat("Input X", 0);
 			animator.SetFloat("Input Z", -0);
 		}
 
-		UpdateMovement();  //update character position and facing
+		//JUMP
+		if (Input.GetButtonDown("Jump") && isControlledLocally || inputJump && !isControlledLocally){
+			if(isStrafing){
+				animator.SetTrigger("JumpTrigger");
+				StartCoroutine(COSetInAir(.3f, .4f));
+			}
+			if(isMoving){
+				animator.SetTrigger("JumpForwardTrigger");
+				StartCoroutine(COSetInAir(0f, 4f));
+			}else{
+		    	animator.SetTrigger("JumpTrigger");
+				StartCoroutine(COSetInAir(.3f, .4f));
+			}
+			if(isControlledLocally) {
+				inputJump = true;
+			}
 
-	}
-
-	void UpdateMovement(){
-		Vector3 motion = inputVec;  //get movement input from controls
-		//reduce input for diagonal movement
-		motion *= (Mathf.Abs(inputVec.x) == 1 && Mathf.Abs(inputVec.z) == 1)?.7f:1;
-
-		if (!isStrafing )
-			RotateTowardMovementDirection();  //if not strafing, face character along input direction
-		if (isStrafing){  //if strafing, look at the target
-			//make character point at target
-			Quaternion targetRotation;
-			Vector3 targetPos = target.transform.position;
-			targetRotation = Quaternion.LookRotation(targetPos - new Vector3(transform.position.x,0,transform.position.z));
-			transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y,targetRotation.eulerAngles.y,(rotationSpeed * Time.deltaTime) * rotationSpeed);
-		}
-		GetCameraRelativeMovement();
-	}
-
-	void GetCameraRelativeMovement(){  //converts control input vectors into camera facing vectors
-
-		Transform cameraTransform = Camera.main.transform;
-		// Forward vector relative to the camera along the x-z plane
-		Vector3 forward = cameraTransform.TransformDirection(Vector3.forward);
-		forward.y = 0;
-		forward = forward.normalized;
-		// Right vector relative to the camera
-		// Always orthogonal to the forward vector
-		Vector3 right= new Vector3(forward.z, 0, -forward.x);
-		//directional inputs
-		float v= Input.GetAxisRaw("Vertical");
-		float h= Input.GetAxisRaw("Horizontal");
-		float dv= Input.GetAxisRaw("DashVertical");
-		float dh= Input.GetAxisRaw("DashHorizontal");
-		// Target direction relative to the camera
-		targetDirection = h * right + v * forward;
-		// Target dash direction relative to the camera
-		targetDashDirection = dh * right + dv * -forward;
-	}
-
-	void RotateTowardMovementDirection(){  //face character along input direction
-		if(!dead){  //if character isn't dead
-			if (inputVec != Vector3.zero && !isStrafing){  //if we're not strafing
-				//take the camera orientated input vector and apply it to our characters facing with smoothing
-				transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetDirection), Time.deltaTime * rotationSpeed);
+			if(inputJump && !isControlledLocally) {
+				inputJump = false;
 			}
 		}
+
+		//ATTACK
+		if (Input.GetButtonDown("Fire0") && isControlledLocally || inputAttack0 && !isControlledLocally){
+			animator.SetTrigger("RangeAttack1Trigger");
+			StartCoroutine (COStunPause(1.2f));
+			if(isControlledLocally) {
+				inputAttack0 = true;
+			}
+
+			if(inputAttack0 && !isControlledLocally) {
+				inputAttack0 = false;
+			}
+		}
+		if (Input.GetButtonDown("Fire1") && isControlledLocally || inputAttack1 && !isControlledLocally){
+			if(!canChain){ //used for characters who can chain attacks to chain to 2nd Attack
+				animator.SetTrigger("Attack1Trigger");
+				StartCoroutine(COChainWindow(.2f, .4f));
+				StartCoroutine (COStunPause(.6f));
+				chain1 = true;
+				chain2 = false;
+			}
+			else{
+				if(chain1){  //if within chain time do ATTACK2
+					animator.SetTrigger("Attack2Trigger");
+					StopAllCoroutines();
+
+					StartCoroutine(COChainWindow(.1f, 2f));
+					chain1 = false;
+					chain2 = true;
+				}
+				else if(chain2){
+					StopAllCoroutines();
+					animator.SetTrigger("Attack3Trigger");
+					chain1 = false;
+					chain2 = false;
+					StartCoroutine (COStunPause(1.2f));
+					canChain = false;
+				}
+			}
+			if(isControlledLocally) {
+				inputAttack1 = true;
+			}
+
+			if(inputAttack1 && !isControlledLocally) {
+				inputAttack1 = false;
+			}
+		}
+
+		if (Input.GetButtonDown("Fire2") && isControlledLocally || inputAttack2 && !isControlledLocally){
+			animator.SetTrigger("MoveAttack1Trigger");
+			StartCoroutine (COStunPause(.9f));
+			if(isControlledLocally) {
+				inputAttack2 = true;
+			}
+
+			if(inputAttack2 && !isControlledLocally) {
+				inputAttack2 = false;
+			}
+		}
+		if (Input.GetButtonDown("Fire3") && isControlledLocally || inputAttack3 && !isControlledLocally){
+			animator.SetTrigger("SpecialAttack1Trigger");
+			StartCoroutine (COStunPause(1.7f));
+			if(isControlledLocally) {
+				inputAttack3 = true;
+			}
+
+			if(inputAttack3 && !isControlledLocally) {
+				inputAttack3 = false;
+			}
+		}
+		if(Input.GetAxis("DashVertical") > .5 || Input.GetAxis("DashVertical") < -.5 || Input.GetAxis("DashHorizontal") > .5 || Input.GetAxis("DashHorizontal") < -.5){
+			StartCoroutine (CODirectionalDash(Input.GetAxis("DashVertical"), Input.GetAxis("DashHorizontal")));
+		}
+
+		UpdateMovement();  //update character position and facing
 	}
+
+	/**
+	 * CROSS SCRIPT FUNCTIONS
+	**/
+
+	public void ApplyDamage(float damageValue) {
+		Debug.Log("ApplyDamage:" + damageValue);
+		WeaponDamage = damageValue;
+		if(meleeHitbox) {
+			meleeHitbox.SetActive(true);
+		}
+	}
+
+	public void TakeDamage(float damageValue) {
+		Debug.Log("TakeDamage:" + damageValue);
+	}
+
+
+	/**
+	 * NETWORK FUNCTIONS
+	**/
+
+	void OnPhotonSerializeView( PhotonStream stream, PhotonMessageInfo info )
+	{
+		//Multiple components need to synchronize values over the network.
+		//The SerializeState methods are made up, but they're useful to keep
+		//all the data separated into their respective components
+		if( stream.isWriting == true )
+		{
+			Debug.Log("WRITE");
+
+		// We own this player: send the others our data
+      //      stream.SendNext(transform.position);
+       //     stream.SendNext(transform.rotation);
+
+            stream.SendNext(inputPos);
+
+			stream.SendNext(inputJump);
+			inputJump = false;
+
+			stream.SendNext(inputAttack0);
+			inputAttack0 = false;
+
+			stream.SendNext(inputAttack1);
+			inputAttack1 = false;
+
+		}
+		else
+		{
+			Debug.Log("READ");
+
+	//		this.transform.position = (Vector3) stream.ReceiveNext();
+      //  	this.transform.rotation = (Quaternion) stream.ReceiveNext();
+
+        	inputPos = (Vector2)stream.ReceiveNext();
+
+			inputJump = (bool)stream.ReceiveNext();
+			inputAttack0 = (bool)stream.ReceiveNext();
+			inputAttack1 = (bool)stream.ReceiveNext();
+		}
+
+		//SerializeState( stream, info );
+		//OtherScript.SerializeState( stream, info );
+	}
+
+
+
+	/**
+	 * HELPER FUNCTIONS
+	**/
 
 	public IEnumerator CODirectionalDash(float x, float v){
 		//check which way the dash is pressed relative to the character facing
@@ -329,7 +386,6 @@ public class ActorController : MonoBehaviour {
 		animator.SetBool("CanChainBool", false);
 	}
 
-
 	GameObject GetClosestEnemy (GameObject[] enemies) {
 	    GameObject bestTarget = null;
 	    float closestDistanceSqr = Mathf.Infinity;
@@ -348,16 +404,50 @@ public class ActorController : MonoBehaviour {
 	}
 
 
-	public void ApplyDamage(float damageValue) {
-		Debug.Log("ApplyDamage:" + damageValue);
-		WeaponDamage = damageValue;
-		if(meleeHitbox) {
-			meleeHitbox.SetActive(true);
+	void UpdateMovement(){
+		Vector3 motion = inputVec;  //get movement input from controls
+		//reduce input for diagonal movement
+		motion *= (Mathf.Abs(inputVec.x) == 1 && Mathf.Abs(inputVec.z) == 1)?.7f:1;
+
+		if (!isStrafing )
+			RotateTowardMovementDirection();  //if not strafing, face character along input direction
+		if (isStrafing){  //if strafing, look at the target
+			//make character point at target
+			Quaternion targetRotation;
+			Vector3 targetPos = target.transform.position;
+			targetRotation = Quaternion.LookRotation(targetPos - new Vector3(transform.position.x,0,transform.position.z));
+			transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y,targetRotation.eulerAngles.y,(rotationSpeed * Time.deltaTime) * rotationSpeed);
 		}
+		GetCameraRelativeMovement();
 	}
 
-	public void TakeDamage(float damageValue) {
-		Debug.Log("TakeDamage:" + damageValue);
+	void GetCameraRelativeMovement(){  //converts control input vectors into camera facing vectors
+		Transform cameraTransform = Camera.main.transform;
+		// Forward vector relative to the camera along the x-z plane
+		Vector3 forward = cameraTransform.TransformDirection(Vector3.forward);
+		forward.y = 0;
+		forward = forward.normalized;
+		// Right vector relative to the camera
+		// Always orthogonal to the forward vector
+		Vector3 right= new Vector3(forward.z, 0, -forward.x);
+		//directional inputs
+		float v= Input.GetAxisRaw("Vertical");
+		float h= Input.GetAxisRaw("Horizontal");
+		float dv= Input.GetAxisRaw("DashVertical");
+		float dh= Input.GetAxisRaw("DashHorizontal");
+		// Target direction relative to the camera
+		targetDirection = h * right + v * forward;
+		// Target dash direction relative to the camera
+		targetDashDirection = dh * right + dv * -forward;
+	}
+
+	void RotateTowardMovementDirection(){  //face character along input direction
+		if(!dead){  //if character isn't dead
+			if (inputVec != Vector3.zero && !isStrafing){  //if we're not strafing
+				//take the camera orientated input vector and apply it to our characters facing with smoothing
+				transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetDirection), Time.deltaTime * rotationSpeed);
+			}
+		}
 	}
 
 }
